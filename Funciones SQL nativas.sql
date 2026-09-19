ce882a9fb18feb269d2c117a5c3ac79d8b54ae91
Tobias-Chep <167262015+Tobias-Chep@users.noreply.github.com>
2026-09-18 22:09:46 -0300

    Add files via upload

---
+++ Funciones SQL nativas.sql
@@ -0,0 +1,8 @@
+# Funciones SQL nativas
+
+# así como en Python tenemos: int(), float(), type()
+# las funciones nativas de SQL son
+# count() se encarga de contar los registros
+# min() se encarga de sustraer el dato  menor
+# avg() saca el promedio
+# suma() agarra todos los valores de un campo (int) y los suma. sirve para campos como stock, precio
